81b53215e7a0436b5fcb54a7d42743d36e6823a2
Bas <bbuijsen@gmail.com>
2021-06-03 10:58:50 +0200

    added graphs

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { ChartOptions, ChartType } from 'chart.js';
-import { Label, monkeyPatchChartJsLegend, monkeyPatchChartJsTooltip, MultiDataSet, SingleDataSet } from 'ng2-charts';
+import { variable } from '@angular/compiler/src/output/output_ast';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { isEmptyObject } from 'jquery';
+import { BaseChartDirective, Color, Label, monkeyPatchChartJsLegend, monkeyPatchChartJsTooltip, MultiDataSet, SingleDataSet } from 'ng2-charts';
 import { Globals } from 'src/globals';
 
 @Component({
@@ -9,7 +11,7 @@ import { Globals } from 'src/globals';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  constructor(public globals: Globals) { 
+  constructor(public globals: Globals) {
     monkeyPatchChartJsTooltip();
     monkeyPatchChartJsLegend();
   }
@@ -25,4 +27,76 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  public lineChartLabels: Label[] = ['1', '2', '3', '4', '5', '6', '7'];
+  public lineChartData: ChartDataSets[] = [
+    { data: [0, 1, 4, 2, 3, 1, 2], label: 'Logins in de app' }
+  ];
+  public lineChartDataOrders: ChartDataSets[] = [
+    { data: [3, 2, 3, 1, 1, 0, 2], label: 'Bestellingen vanuit de app' }
+  ];
+  public lineChartType: ChartType = 'line';
+  public lineChartLegend = true;
+  public lineChartPlugins = [];
+  public lineChartColors: Color[] = [
+    { // grey
+      backgroundColor: 'rgba(244,151,1, 0)',
+      borderColor: 'rgb(244,151,1)',
+      // pointBackgroundColor: 'rgba(244,151,1, 0)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgb(244,151,1)'
+    },
+    { // dark grey
+      backgroundColor: 'rgba(77,83,96,0.2)',
+      borderColor: 'rgba(77,83,96,1)',
+      pointBackgroundColor: 'rgba(77,83,96,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(77,83,96,1)'
+    },
+    { // red
+      backgroundColor: 'rgba(255,0,0,0.3)',
+      borderColor: 'red',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    }
+  ];
+
+  public lineChartOptions: (ChartOptions & { annotation: any }) = {
+    responsive: true,
+    scales: {
+      // We use this empty structure as a placeholder for dynamic theming.
+      xAxes: [{
+        id: 'x-axis-0',
+        position: 'bottom',
+      }],
+      yAxes: [
+        {
+          id: 'y-axis-0',
+          position: 'left',
+        }
+      ]
+    },
+    annotation: {
+      annotations: [
+        {
+          type: 'line',
+          mode: 'vertical',
+          scaleID: 'x-axis-0',
+          value: 'March',
+          borderColor: 'orange',
+          borderWidth: 2,
+          label: {
+            enabled: true,
+            fontColor: 'orange',
+            content: 'LineAnno'
+          }
+        },
+      ],
+    },
+  };
+
 }
