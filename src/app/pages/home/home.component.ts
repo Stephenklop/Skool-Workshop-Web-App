0f05375ac0b1a2566c7f1f404057e01b88bbbe0f
Bas <bbuijsen@gmail.com>
2021-06-03 16:35:31 +0200

    added notification ui

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
+import { analyzeAndValidateNgModules } from '@angular/compiler';
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { Filter } from 'angular-feather/icons';
 import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
 import * as faker from 'faker';
 import { BaseChartDirective, Color, Label, monkeyPatchChartJsLegend, monkeyPatchChartJsTooltip, MultiDataSet, SingleDataSet } from 'ng2-charts';
@@ -24,25 +26,60 @@ export class HomeComponent implements OnInit {
   public pieChartLegend = true;
   public pieChartPlugins = [];
   public user: any = undefined;
+  public notificationToEveryone: boolean = false;
 
   ngOnInit(): void {
     this.generateFakeData();
+    this.globals.notification_used_list = this.globals.notification_test_list;
+    document.getElementById("send")!.addEventListener("click", function (event) {
+      event.preventDefault()
+    });
+
+
   }
 
-  selectUser(name: String, email: String) {
+  filterList(searchKey: String, testList: any, globals: any) {
+    let filteredList: any = []
+    testList.forEach((value: any) => {
+      let name: String = value.name
+      if (name.toLowerCase().includes(searchKey.toLowerCase())) {
+        filteredList.push(value)
+      }
+    });
+    globals.notification_used_list = filteredList;
+  }
+
+  startFilter() {
+    let searchBar = <HTMLInputElement>document.getElementById("search")!;
+
+    const flist = this.filterList;
+    const global = this.globals;
+
+    searchBar.addEventListener("keyup", function (event) {
+      console.log(event);
+      flist(searchBar.value, global.notification_test_list, global);
+    });
+  }
+
+
+
+  selectUser(id: Number, name: String, email: String) {
     console.log(name)
     console.log(email)
-    this.testList.forEach(function (value: any) {
-      document.getElementById("userButton" + value.name)!.style.backgroundColor = "#f8f8f8"
-      document.getElementById("userButton" + value.name)!.style.color = "#707070"
-      document.getElementById("userButton" + value.name)!.style.border = "2px solid #707070"
+    this.globals.notification_test_list.forEach(function (value: any) {
+      if (document.getElementById("userButton" + value.id) != null) {
+        document.getElementById("userButton" + value.id)!.style.backgroundColor = "#f8f8f8"
+        document.getElementById("userButton" + value.id)!.style.color = "#707070"
+        document.getElementById("userButton" + value.id)!.style.border = "2px solid #707070"
+
+      }
     });
     try {
-      document.getElementById("userButton" + name)!.style.backgroundColor = "#F49700"
-      document.getElementById("userButton" + name)!.style.color = "#fff"
-      document.getElementById("userButton" + name)!.style.border = "none"
+      document.getElementById("userButton" + id)!.style.backgroundColor = "#F49700"
+      document.getElementById("userButton" + id)!.style.color = "#fff"
+      document.getElementById("userButton" + id)!.style.border = "none"
 
-      this.user = { name, email }
+      this.user = { id, name, email }
     } catch (e) {
       console.log(e)
     }
@@ -52,13 +89,68 @@ export class HomeComponent implements OnInit {
   userSelected() {
     console.log("clicked on next")
     if (this.user != undefined) {
-      document.getElementById("list")!.style.visibility = "hidden"
+      document.getElementById("list")!.style.display = "none";
+      document.getElementById("next")!.style.display = "none";
+      document.getElementById("search")!.style.display = "none";
+      document.getElementById("icon_search")!.style.display = "none";
+      document.getElementById("list")!.style.zIndex = "0";
+      document.getElementById("home__other-input")!.style.display = "block";
+
     } else {
       console.log("geen user geselecteerd")
     }
   }
 
-  public testList: any = [];
+  sendNotification() {
+    let titleInput = <HTMLInputElement>document.getElementById("title")
+    let title: any = titleInput.value
+    let descriptionInput = <HTMLInputElement>document.getElementById("description")
+    let description = descriptionInput.value
+    console.log(title);
+    console.log(description)
+
+    if (title != "" && description != "") {
+      console.log("send notification to: " + this.user.name + " (" + this.user.email + ")\n" + "With title: " + title + "\nand description: " + description + ";")
+
+      this.globals.notification_test_list.forEach(function (value: any) {
+        if (document.getElementById("userButton" + value.id) != null) {
+          document.getElementById("userButton" + value.id)!.style.backgroundColor = "#f8f8f8"
+          document.getElementById("userButton" + value.id)!.style.color = "#707070"
+          document.getElementById("userButton" + value.id)!.style.border = "2px solid #707070"
+        }
+      });
+
+      titleInput.value = ""
+      descriptionInput.value = ""
+
+      this.user = undefined
+
+      document.getElementById("home__other-input")!.style.display = "none";
+      document.getElementById("next")!.style.display = "block";
+      document.getElementById("search")!.style.display = "block";
+      document.getElementById("icon_search")!.style.display = "block";
+      document.getElementById("list")!.style.zIndex = "1";
+      document.getElementById("home__other-input")!.style.zIndex = "1";
+      document.getElementById("list")!.style.display = "flex";
+
+    }
+
+  }
+
+  switchNotificationGroup() {
+    if (this.notificationToEveryone) {
+      document.getElementById("everyoneNotification")!.style.display = "none"
+      document.getElementById("onePersonNotifications")!.style.display = "flex"
+      this.notificationToEveryone = false
+      document.getElementById("switchButton")!.textContent = "Een persoon"
+    } else {
+      document.getElementById("everyoneNotification")!.style.display = "flex"
+      document.getElementById("onePersonNotifications")!.style.display = "none"
+      this.notificationToEveryone = true
+      document.getElementById("switchButton")!.textContent = "Iedereen"
+    }
+  }
+
 
   public lineChartLabels: Label[] = ['1', '2', '3', '4', '5', '6', '7'];
   public lineChartData: ChartDataSets[] = [
@@ -132,13 +224,12 @@ export class HomeComponent implements OnInit {
   };
 
   generateFakeData() {
-    let id = 1;
 
     for (let i = 0; i < 30; i++) {
-      this.testList.push({
+      this.globals.notification_test_list.push({
+        id: i + 1,
         name: faker.name.firstName() + " " + faker.name.lastName(),
         email: faker.name.firstName() + "" + faker.name.lastName() + "@gmail.com"
-
       });
       console.log("user " + i + " is gemaakt")
     }
--- src/globals.ts
+++ src/globals.ts
@@ -3,8 +3,11 @@ import { Injectable } from '@angular/core';
 @Injectable()
 
 export class Globals {
-    loader_finished                                 : boolean       = true;
+    loader_finished: boolean = true;
 
-    page_login_active                               : boolean       = false;
-    page_forgotpass_active                          : boolean       = false;
+    page_login_active: boolean = false;
+    page_forgotpass_active: boolean = false;
+
+    notification_test_list: Array<any> = [];
+    notification_used_list: Array<any> = [];
 }
